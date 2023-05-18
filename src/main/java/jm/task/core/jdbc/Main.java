f74a333b250c5dddb37b945877adc0f04d76c38c
yourock <awlwlb@yzndex.ru>
2023-05-18 16:16:43 +0400

    PP 1.1.4

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,22 +4,32 @@ import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
 
+import java.sql.SQLException;
+import java.sql.Savepoint;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Util.getConnection().setAutoCommit(false);
+        Savepoint savepoint = Util.getConnection().setSavepoint();
 
-        Util.getConnection();
-        UserDao userDao = new UserDaoJDBCImpl();
+        try {
+            UserDao userDao = new UserDaoJDBCImpl();
 
-        userDao.createUsersTable();
+            userDao.createUsersTable();
 
-        userDao.saveUser("Borat", "Sagdiyev", (byte)45);
-        userDao.saveUser("Azamat", "Bagatov", (byte) 55);
-        userDao.saveUser("Natalya", "Sagdiyeva", (byte) 40);
-        userDao.saveUser("Pamela", "Anderson", (byte) 55);
+            userDao.saveUser("Borat", "Sagdiyev", (byte)45);
+            userDao.saveUser("Azamat", "Bagatov", (byte) 55);
+            userDao.saveUser("Natalya", "Sagdiyeva", (byte) 40);
+            userDao.saveUser("Pamela", "Anderson", (byte) 55);
 
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+            userDao.getAllUsers();
+            userDao.cleanUsersTable();
+            userDao.dropUsersTable();
+        } catch (Exception e) {
+            Util.getConnection().rollback(savepoint);
+        }
+
+        Util.getConnection().commit();
     }
 }
